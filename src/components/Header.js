de308e84487dfe863d8d909ced36f6c8f7381eae
Natanael Bezerra <nsbbezerra@hotmail.com>
2022-01-17 18:12:43 -0300

    remove window buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import { ArrowsExpandIcon, MinusIcon, XIcon } from "@heroicons/react/outline";
 import { Fragment } from "react";
 
 export default function HeaderButtons({ title, icon }) {
@@ -8,17 +7,6 @@ export default function HeaderButtons({ title, icon }) {
         <div className="h-full flex items-center px-5 text-green-500">
           <div className="w-5 h-5">{icon}</div>
           <span className="ml-3 font-bold text-lg">{title}</span>
-        </div>
-        <div className="flex justify-center items-center rounded-bl-lg overflow-hidden">
-          <button className="px-2 py-2 bg-gray-200 hover:bg-gray-300 active:bg-gray-200">
-            <MinusIcon className="w-4 h-4" />
-          </button>
-          <button className="px-2 py-2 bg-gray-200 hover:bg-gray-300 active:bg-gray-200">
-            <ArrowsExpandIcon className="w-4 h-4" />
-          </button>
-          <button className="px-2 py-2 bg-red-500 hover:bg-red-600 active:bg-red-500">
-            <XIcon className="w-4 h-4" />
-          </button>
         </div>
       </div>
     </Fragment>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,16 @@ import { HomeIcon } from "@heroicons/react/outline";
 import { Fragment } from "react";
 import HeaderButtons from "../components/Header";
 
+import Logo from "../assets/logo.png";
+
 export default function Index() {
   return (
     <Fragment>
       <HeaderButtons title={"DASHBOARD"} icon={<HomeIcon />} />
+
+      <div className="flex justify-center items-center pt-32 px-3">
+        <img src={Logo} alt="PA Rifas" className="w-2/5 block" />
+      </div>
     </Fragment>
   );
 }
